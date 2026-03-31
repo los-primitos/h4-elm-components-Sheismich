4187057f430c4629f566407661898ce91c2f9c8c
Carlos Emanuel Muñiz <up230488@alumnos.upa.edu.mx>
2026-03-30 21:51:55 -0600

    Tarea 4 sheismich

--- src/Helper.elm
+++ src/Helper.elm
@@ -5,7 +5,38 @@ import Html.Attributes
 
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html.Html msg
+headers param =
+    Html.div []
+        [ Html.h1 [] [ Html.text param ]
+        , Html.h2 [] [ Html.text param ]
+        , Html.h3 [] [ Html.text param ]
+        , Html.h4 [] [ Html.text param ]
+        , Html.h5 [] [ Html.text param ]
+        , Html.h6 [] [ Html.text param ]
+        ]
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink url text =
+    Html.a [ Html.Attributes.href url ] [ Html.text text ]
+
+
+main : Html.Html msg
+main =
+    Html.div [] [ headers "Hola Mundo", hyperlink "https://upa.edu.mx/" "Inscribete" ]
